cfb96a417220cc13b67d2d968fc4b8b34a0974ee
Erik Madsen <beatmadsen@gmail.com>
2024-12-07 13:21:38 +0100

    extracting service to own file

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -56,29 +56,3 @@ func newHandler() http.Handler {
 		service: &servicePrototype{},
 	}
 }
-
-type service interface {
-	advanceSlow(revision string) error
-	advanceFast(revision string) error
-	state(revision string) (state, error)
-}
-
-type state struct {
-	Revision string `json:"revision"`
-	Fast     uint8  `json:"fast"`
-	Slow     uint8  `json:"slow"`
-}
-
-type servicePrototype struct{}
-
-func (s *servicePrototype) advanceSlow(revision string) error {
-	return nil
-}
-
-func (s *servicePrototype) advanceFast(revision string) error {
-	return nil
-}
-
-func (s *servicePrototype) state(revision string) (state, error) {
-	return state{}, nil
-}

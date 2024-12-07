eca4610ce80de5aae5700dc2d09e7acd9bbd5f22
Erik Madsen <beatmadsen@gmail.com>
2024-12-07 22:29:39 +0100

    All states ready in state machine

--- internal/server/state_machine/state_machine.go
+++ internal/server/state_machine/state_machine.go
@@ -6,16 +6,18 @@ type stateMachine struct {
 	currentState state
 }
 
-func newStateMachine(state string) (*stateMachine, error) {
-	switch state {
+func newStateMachine(initialState string) (*stateMachine, error) {
+	switch initialState {
 	case "new":
 		return &stateMachine{currentState: &newState{}}, nil
+	case "environment ready":
+		return &stateMachine{currentState: &environmentReadyState{}}, nil
 	case "building":
 		return &stateMachine{currentState: &buildingState{}}, nil
 	case "testing":
 		return &stateMachine{currentState: &testingState{}}, nil
 	default:
-		return nil, fmt.Errorf("invalid state: %s", state)
+		return nil, fmt.Errorf("invalid state: %s", initialState)
 	}
 }
 
@@ -23,22 +25,18 @@ func (s *stateMachine) CurrentState() string {
 	return s.currentState.String()
 }
 
-func (s *stateMachine) advance() error {
-	state, err := s.currentState.advance()
-	if err != nil {
-		return err
-	}
+func (s *stateMachine) advance() {
+	state := s.currentState.advance()
+
 	s.currentState = state
-	return nil
 }
 
-func (s *stateMachine) fail(err error) error {
+func (s *stateMachine) fail(err error) {
 	s.currentState = s.currentState.fail(err)
-	return nil
 }
 
 type state interface {
-	advance() (state, error)
+	advance() state
 	fail(error) state
 	String() string
 }
@@ -47,8 +45,8 @@ type failedState struct {
 	err error
 }
 
-func (s *failedState) advance() (state, error) {
-	return nil, fmt.Errorf("Cannot advance from failed state: %s", s.err)
+func (s *failedState) advance() state {
+	return s
 }
 
 func (s *failedState) fail(err error) state {
@@ -61,8 +59,8 @@ func (s *failedState) String() string {
 
 type newState struct{}
 
-func (s *newState) advance() (state, error) {
-	return &buildingState{}, nil
+func (s *newState) advance() state {
+	return &environmentReadyState{}
 }
 
 func (s *newState) fail(err error) state {
@@ -73,10 +71,24 @@ func (s *newState) String() string {
 	return "new"
 }
 
+type environmentReadyState struct{}
+
+func (s *environmentReadyState) advance() state {
+	return &buildingState{}
+}
+
+func (s *environmentReadyState) fail(err error) state {
+	return &failedState{err: err}
+}
+
+func (s *environmentReadyState) String() string {
+	return "environment ready"
+}
+
 type buildingState struct{}
 
-func (s *buildingState) advance() (state, error) {
-	return &testingState{}, nil
+func (s *buildingState) advance() state {
+	return &testingState{}
 }
 
 func (s *buildingState) fail(err error) state {
@@ -89,8 +101,8 @@ func (s *buildingState) String() string {
 
 type testingState struct{}
 
-func (s *testingState) advance() (state, error) {
-	return nil, nil
+func (s *testingState) advance() state {
+	return &succeededState{}
 }
 
 func (s *testingState) fail(err error) state {
@@ -99,4 +111,18 @@ func (s *testingState) fail(err error) state {
 
 func (s *testingState) String() string {
 	return "testing"
+}
+
+type succeededState struct{}
+
+func (s *succeededState) advance() state {
+	return s
+}
+
+func (s *succeededState) fail(err error) state {
+	return s
+}
+
+func (s *succeededState) String() string {
+	return "succeeded"
 }
--- internal/server/state_machine/state_machine_test.go
+++ internal/server/state_machine/state_machine_test.go
@@ -1,11 +1,14 @@
 package statemachine
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestCreatingMachineForRevision(t *testing.T) {
 	machine, err := newStateMachine("new")
 	if err != nil {
-		t.Error("Expected no error")
+		t.Fatal("Expected no error")
 	}
 	if machine.CurrentState() != "new" {
 		t.Error("Expected current state to be 'new'")
@@ -15,7 +18,18 @@ func TestCreatingMachineForRevision(t *testing.T) {
 func TestAdvancingNewState(t *testing.T) {
 	machine, err := newStateMachine("new")
 	if err != nil {
-		t.Error("Expected no error")
+		t.Fatal("Expected no error")
+	}
+	machine.advance()
+	if machine.CurrentState() != "environment ready" {
+		t.Error("Expected current state to be 'environment ready'")
+	}
+}
+
+func TestAdvancingEnvironmentReadyState(t *testing.T) {
+	machine, err := newStateMachine("environment ready")
+	if err != nil {
+		t.Fatal("Expected no error")
 	}
 	machine.advance()
 	if machine.CurrentState() != "building" {
@@ -26,10 +40,64 @@ func TestAdvancingNewState(t *testing.T) {
 func TestAdvancingBuildingState(t *testing.T) {
 	machine, err := newStateMachine("building")
 	if err != nil {
-		t.Error("Expected no error")
+		t.Fatal("Expected no error")
 	}
 	machine.advance()
 	if machine.CurrentState() != "testing" {
 		t.Error("Expected current state to be 'testing'")
+	}
+}
+
+func TestAdvancingTestingState(t *testing.T) {
+	machine, err := newStateMachine("testing")
+	if err != nil {
+		t.Fatal("Expected no error")
+	}
+	machine.advance()
+	if machine.CurrentState() != "succeeded" {
+		t.Error("Expected current state to be 'succeeded'")
+	}
+}
+
+func TestFailingNewState(t *testing.T) {
+	machine, err := newStateMachine("new")
+	if err != nil {
+		t.Fatal("Expected no error")
+	}
+	machine.fail(errors.New("test error"))
+	if machine.CurrentState() != "failed" {
+		t.Error("Expected current state to be 'failed'")
+	}
+}
+func TestFailingEnvironmentReadyState(t *testing.T) {
+	machine, err := newStateMachine("environment ready")
+	if err != nil {
+		t.Fatal("Expected no error")
+	}
+	machine.fail(errors.New("test error"))
+	if machine.CurrentState() != "failed" {
+		t.Error("Expected current state to be 'failed'")
+	}
+}
+
+func TestFailingBuildingState(t *testing.T) {
+	machine, err := newStateMachine("building")
+	if err != nil {
+		t.Fatal("Expected no error")
+	}
+	machine.fail(errors.New("test error"))
+	if machine.CurrentState() != "failed" {
+		t.Error("Expected current state to be 'failed'")
+	}
+}
+
+func TestFailingTestingState(t *testing.T) {
+	machine, err := newStateMachine("testing")
+	if err != nil {
+		t.Fatal("Expected no error")
+	}
+	machine.fail(errors.New("test error"))
+	if machine.CurrentState() != "failed" {
+		t.Error("Expected current state to be 'failed'")
 	}
 }

998701aa22fd96329500d0dbec9bf76b86767709
Erik Madsen <beatmadsen@gmail.com>
2024-12-08 10:46:41 +0100

    Injecting db into service

--- internal/server/service/service_test.go
+++ internal/server/service/service_test.go
@@ -1,11 +1,57 @@
 package service
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/beatmadsen/left-ci/internal/db"
+)
 
 func TestAdvanceSlowWhenDbFails(t *testing.T) {
 
 }
 
+func newService() Service {
+	return &servicePrototype{db: &dbStub{}}
+}
+
 type dbStub struct {
 	failing bool
+}
+
+func (d *dbStub) maybeFail() error {
+	if d.failing {
+		return fmt.Errorf("failed")
+	}
+	return nil
+}
+
+func (d *dbStub) Close() error {
+	return d.maybeFail()
+}
+
+func (d *dbStub) CreateRevision(revision string) error {
+	return d.maybeFail()
+}
+
+func (d *dbStub) FastState(revision string) (*db.State, error) {
+	if d.failing {
+		return nil, fmt.Errorf("failed")
+	}
+	return &db.State{State: "environment ready", Revision: revision}, nil
+}
+
+func (d *dbStub) SlowState(revision string) (*db.State, error) {
+	if d.failing {
+		return nil, fmt.Errorf("failed")
+	}
+	return &db.State{State: "environment ready", Revision: revision}, nil
+}
+
+func (d *dbStub) UpdateFastState(revision, state string) error {
+	return d.maybeFail()
+}
+
+func (d *dbStub) UpdateSlowState(revision, state string) error {
+	return d.maybeFail()
 }

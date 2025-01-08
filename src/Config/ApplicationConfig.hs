c80a614b85b0602daf7ea507eb2a02723bfdd30a
Erik T. Madsen <beatmadsen@gmail.com>
2025-01-09 00:28:59 +0100

    Validating args

--- src/Config/ApplicationConfig.hs
+++ src/Config/ApplicationConfig.hs
@@ -13,6 +13,7 @@ data ApplicationConfig
 parseApplicationConfig :: [String] -> ApplicationConfig
 parseApplicationConfig [] = Invalid "No arguments provided"
 parseApplicationConfig ("--server" : rest) = parseServer rest
+parseApplicationConfig ("--installer" : rest) = parseInstaller rest
 parseApplicationConfig _ = error "Not implemented yet"
 
 parseServer :: [String] -> ApplicationConfig
@@ -26,3 +27,10 @@ parsePort :: String -> ApplicationConfig
 parsePort port = case reads port :: [(Int, String)] of
   [(n, "")] | n >= 0 && n <= 65535 -> Server n
   _ -> Invalid $ "Invalid port number: " ++ port ++ ". Please add a number after --port"
+
+parseInstaller :: [String] -> ApplicationConfig
+parseInstaller [] = Invalid "No path provided to the project where you want to install Left CI. Please add a path after --installer using --path <path>"
+parseInstaller ("--path" : rest) = case rest of
+  [] -> Invalid "No path provided to the project where you want to install Left CI. Please add a path after --installer using --path <path>"
+  (path : _) -> Installer path
+parseInstaller _ = Invalid "Invalid installer configuration"
--- test/unit/Config/ApplicationConfigTest.hs
+++ test/unit/Config/ApplicationConfigTest.hs
@@ -4,7 +4,7 @@ module Config.ApplicationConfigTest
 where
 
 import Config.ApplicationConfig
-  ( ApplicationConfig (Invalid, Server),
+  ( ApplicationConfig (Invalid, Server, Installer),
     parseApplicationConfig,
   )
 import Test.HUnit (Test (TestCase, TestLabel, TestList), (@?=))
@@ -31,5 +31,17 @@ tests =
       TestLabel "server command with port flag sets up server with port" $ TestCase $ do
         let actual = parseApplicationConfig ["--server", "--port", "8080"]
         let expected = Server 8080
+        actual @?= expected,
+      TestLabel "server command with port flag fails if port is outside of 0-65535" $ TestCase $ do
+        let actual = parseApplicationConfig ["--server", "--port", "65536"]
+        let expected = Invalid "Invalid port number: 65536. Please add a number after --port"
+        actual @?= expected,
+      TestLabel "installer command without path fails" $ TestCase $ do
+        let actual = parseApplicationConfig ["--installer"]
+        let expected = Invalid "No path provided to the project where you want to install Left CI. Please add a path after --installer using --path <path>"
+        actual @?= expected,
+      TestLabel "installer command with path sets up installer with path" $ TestCase $ do
+        let actual = parseApplicationConfig ["--installer", "--path", "/path/to/project"]
+        let expected = Installer "/path/to/project"
         actual @?= expected
     ]

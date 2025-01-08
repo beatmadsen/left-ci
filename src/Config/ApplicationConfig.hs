e5e4804665f50883ed2ef308b7a51897d334de11
Erik T. Madsen <beatmadsen@gmail.com>
2025-01-08 16:23:23 +0100

    Validating args

--- src/Config/ApplicationConfig.hs
+++ src/Config/ApplicationConfig.hs
@@ -1,7 +1,8 @@
 module Config.ApplicationConfig
-  ( ApplicationConfig(..)
-  , parseApplicationConfig
-  ) where
+  ( ApplicationConfig (..),
+    parseApplicationConfig,
+  )
+where
 
 data ApplicationConfig
   = Server Int
@@ -10,7 +11,13 @@ data ApplicationConfig
   deriving (Show, Eq)
 
 parseApplicationConfig :: [String] -> ApplicationConfig
-
 parseApplicationConfig [] = Invalid "No arguments provided"
-parseApplicationConfig ["--server"] = Invalid "No port number provided to run the server on. Please add --port <port>"
+parseApplicationConfig ("--server" : rest) = parseServerConfig rest
 parseApplicationConfig _ = error "Not implemented yet"
+
+parseServerConfig :: [String] -> ApplicationConfig
+parseServerConfig [] = Server 8585 -- Case for just "--server"
+parseServerConfig ("--port" : rest) = case rest of
+  [] -> Invalid "No port number provided to run the server on. Please add a number after --port"
+  (port : _) -> Invalid $ "Invalid port number: " ++ port ++ ". Please add a number after --port"
+parseServerConfig _ = Invalid "Invalid server configuration"
--- test/unit/Config/ApplicationConfigTest.hs
+++ test/unit/Config/ApplicationConfigTest.hs
@@ -1,19 +1,31 @@
 module Config.ApplicationConfigTest
-  ( tests
-  ) where
+  ( tests,
+  )
+where
 
-import Test.HUnit ( (@?=), Test(TestCase, TestList) )
 import Config.ApplicationConfig
-    ( parseApplicationConfig, ApplicationConfig(Invalid) )
+  ( ApplicationConfig (Invalid, Server),
+    parseApplicationConfig,
+  )
+import Test.HUnit (Test (TestCase, TestLabel, TestList), (@?=))
 
 tests :: Test
-tests = TestList
-  [ TestCase $ do
-      let actual = parseApplicationConfig []
-      let expected = Invalid "No arguments provided"
-      actual @?= expected,
-    TestCase $ do
-      let actual = parseApplicationConfig ["--server"]
-      let expected = Invalid "No port number provided to run the server on. Please add --port <port>"
-      actual @?= expected
-  ]
+tests =
+  TestList
+    [ TestCase $ do
+        let actual = parseApplicationConfig []
+        let expected = Invalid "No arguments provided"
+        actual @?= expected,
+      TestLabel "server command without port flag defaults port to 8585" $ TestCase $ do
+        let actual = parseApplicationConfig ["--server"]
+        let expected = Server 8585
+        actual @?= expected,
+      TestCase $ do
+        let actual = parseApplicationConfig ["--server", "--port"]
+        let expected = Invalid "No port number provided to run the server on. Please add a number after --port"
+        actual @?= expected,
+      TestLabel "server command with port flag fails if port is not a number" $ TestCase $ do
+        let actual = parseApplicationConfig ["--server", "--port", "not-a-number"]
+        let expected = Invalid "Invalid port number: not-a-number. Please add a number after --port"
+        actual @?= expected
+    ]

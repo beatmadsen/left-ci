62cb37203477114e0138f2b96f8d2d9193ee36c4
Erik T. Madsen <beatmadsen@gmail.com>
2025-01-20 02:59:46 +0100

    Parse whole body

--- src/Server/Routes.hs
+++ src/Server/Routes.hs
@@ -5,15 +5,17 @@ module Server.Routes
     ( makeApplication
     ) where
 
-import Web.Scotty (ScottyM, get, post, json, pathParam, request)
+import Web.Scotty 
+    ( ScottyM, get, post, json, jsonData, pathParam, ActionM, status
+    , catch)
+import Web.Scotty.Internal.Types (ActionError)
+import Network.HTTP.Types.Status (status500)
 import Control.Monad.IO.Class (liftIO)
-import Data.Aeson (FromJSON(..), withObject, (.:), decode)
+import Data.Aeson (FromJSON(..), withObject, (.:))
 import Data.Aeson.Types (Parser)
 import Server.BuildService (BuildService(..), BuildId)
 import Server.Domain (TestResult(..))
 import Data.Text (Text)
-import qualified Data.ByteString.Lazy as LBS
-import Network.Wai (getRequestBodyChunk)
 
 data ResultRequest = ResultRequest 
     { result :: TestResult 
@@ -26,6 +28,15 @@ instance FromJSON ResultRequest where
             "success" -> SuccessResult
             _ -> FailureResult
 
+handleJsonError :: ActionError -> ActionM a
+handleJsonError e = do
+    status status500
+    json $ "Failed to parse JSON: " ++ show e
+    error "JSON parse failed"
+
+parseResultRequest :: ActionM ResultRequest
+parseResultRequest = jsonData `catch` handleJsonError
+
 makeApplication :: BuildService -> ScottyM ()
 makeApplication service = do
     get "/build/:id" $ do
@@ -35,10 +46,6 @@ makeApplication service = do
         
     post "/build/:id/fast" $ do
         buildId <- pathParam "id"
-        waiRequest <- request
-        chunk <- liftIO $ getRequestBodyChunk waiRequest
-        case decode (LBS.fromStrict chunk) of
-            Nothing -> error "Failed to parse JSON"
-            Just (ResultRequest{result}) -> do
-                liftIO $ setFastResult service buildId result
-                json ()
+        request <- parseResultRequest
+        liftIO $ setFastResult service buildId (result request)
+        json ()

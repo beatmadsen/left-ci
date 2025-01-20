b16f4261b68b69a764b251c85df27ca17462ef8d
Erik T. Madsen <beatmadsen@gmail.com>
2025-01-20 02:10:44 +0100

    Cleaning test

--- test/integration/Server/ServiceTest.hs
+++ test/integration/Server/ServiceTest.hs
@@ -5,26 +5,19 @@ module Server.ServiceTest
   )
 where
 
-import qualified Data.ByteString.Lazy as LBS
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.IORef as IORef
+import Data.Text (pack)
 import Network.HTTP.Types
 import Network.HTTP.Types.Header (hContentType)
-import Network.Wai (Application, pathInfo, requestBody, requestHeaders, requestMethod, setRequestBodyChunks)
+import Network.Wai (Application, Request, pathInfo, requestBody, requestHeaders, requestMethod, setRequestBodyChunks)
 import Network.Wai.Test (Session, assertBody, assertStatus, defaultRequest, request, runSession)
 import Server.BuildService
 import Server.Domain
 import Server.Routes
 import Test.HUnit
 import Web.Scotty (scottyApp)
-import Control.Monad.IO.Class (liftIO)
-
-makeStubService :: IO BuildService
-makeStubService =
-  pure
-    BuildService
-      { getBuildStatus = \_ -> pure $ BuildStatus Nothing Nothing,
-        setFastResult = \_ _ -> pure ()
-      }
 
 testGetStatus :: Test
 testGetStatus = TestCase $ do
@@ -41,7 +34,6 @@ testGetStatus = TestCase $ do
 
 testUsesPathBuildId :: Test
 testUsesPathBuildId = TestCase $ do
-  -- Create an IORef to store the ID that was passed to the service
   passedId <- IORef.newIORef ""
 
   let service =
@@ -49,12 +41,11 @@ testUsesPathBuildId = TestCase $ do
           { getBuildStatus = \id -> do
               IORef.writeIORef passedId id
               pure $ BuildStatus Nothing Nothing,
-            setFastResult = \_ _ -> pure ()
+            setFastResult = undefined
           }
 
   app <- scottyApp $ makeApplication service
 
-  -- Make request with specific build ID
   runSession
     ( do
         response <-
@@ -66,46 +57,58 @@ testUsesPathBuildId = TestCase $ do
     )
     app
 
-  -- Verify the service was called with correct ID
   actualId <- IORef.readIORef passedId
   assertEqual "Build ID" "test-build-42" actualId
 
 testPostFastResult :: Test
 testPostFastResult = TestCase $ do
-    passedId <- IORef.newIORef ""
-    passedResult <- IORef.newIORef Nothing
-    
-    let service = BuildService
-            { getBuildStatus = \_ -> pure $ BuildStatus Nothing Nothing
-            , setFastResult = \id result -> do
-                IORef.writeIORef passedId id
-                IORef.writeIORef passedResult (Just result)
-                pure ()
-            }
-            
-    app <- scottyApp $ makeApplication service
-    
-    runSession (do
-        let jsonBody = "{\"result\": \"success\"}"
-        let req = setRequestBodyChunks (pure $ LBS.toStrict jsonBody) $
-                defaultRequest 
-                    { pathInfo = ["build", "test-build-42", "fast"]
-                    , requestMethod = "POST"
-                    , requestHeaders = [(hContentType, "application/json")]
-                    }
-        response <- request req
+  passedId <- IORef.newIORef ""
+  passedResult <- IORef.newIORef Nothing
+
+  let service =
+        BuildService
+          { getBuildStatus = undefined,
+            setFastResult = \id result -> do
+              IORef.writeIORef passedId id
+              IORef.writeIORef passedResult (Just result)
+              pure ()
+          }
+
+  app <- scottyApp $ makeApplication service
+
+  runSession
+    ( do
+        response <- request $ makePostRequest "test-build-42" "{\"result\": \"success\"}"
         assertStatus 200 response
-        ) app
-        
-    actualId <- IORef.readIORef passedId
-    actualResult <- IORef.readIORef passedResult
-    assertEqual "Build ID" "test-build-42" actualId
-    assertEqual "Result" (Just SuccessResult) actualResult
+    )
+    app
+
+  actualId <- IORef.readIORef passedId
+  actualResult <- IORef.readIORef passedResult
+  assertEqual "Build ID" "test-build-42" actualId
+  assertEqual "Result" (Just SuccessResult) actualResult
+
+makePostRequest :: String -> String -> Request
+makePostRequest buildId jsonBody =
+  setRequestBodyChunks (pure $ LBS.toStrict $ LBS.pack jsonBody) $
+    defaultRequest
+      { pathInfo = ["build", pack buildId, "fast"],
+        requestMethod = "POST",
+        requestHeaders = [(hContentType, "application/json")]
+      }
+
+makeStubService :: IO BuildService
+makeStubService =
+  pure
+    BuildService
+      { getBuildStatus = \_ -> pure $ BuildStatus Nothing Nothing,
+        setFastResult = \_ _ -> pure ()
+      }
 
 tests :: Test
 tests =
   TestList
-    [ TestLabel "Get status endpoint" testGetStatus,
-      TestLabel "Uses build ID from path" testUsesPathBuildId,
-      TestLabel "Post fast result" testPostFastResult
+    [ TestLabel "Given a build ID, when getting status, then returns empty status" testGetStatus,
+      TestLabel "Given a build ID in URL, when getting status, then passes ID to service" testUsesPathBuildId,
+      TestLabel "Given a build ID, when posting success result, then updates fast result" testPostFastResult
     ]

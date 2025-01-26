03d1185b045f3a2f244e49e48fad334a47e3ba59
Erik T. Madsen <beatmadsen@gmail.com>
2025-01-26 11:22:01 +0100

    Failing persistence test

--- src/Server/DataStore.hs
+++ src/Server/DataStore.hs
@@ -1,6 +1,18 @@
 module Server.DataStore
   ( BuildStore (..),
+    BuildRow (..)
   )
 where
 
-data BuildStore = BuildStore { demo :: Int -> String }
+import Data.Text (Text)
+
+data BuildRow = BuildRow
+  { buildId :: Text
+  , versionId :: Text
+  , cadence :: Text  -- "slow" or "fast"
+  , state :: Text    -- raw state string from DB
+  }
+
+data BuildStore = BuildStore 
+  { getBuildRows :: Text -> IO [BuildRow]  -- Takes a build ID, returns matching rows
+  }
--- test/unit/Server/Service/PersistentTest.hs
+++ test/unit/Server/Service/PersistentTest.hs
@@ -5,7 +5,7 @@ module Server.Service.PersistentTest
   )
 where
 
-import Server.DataStore (BuildStore (..))
+import Server.DataStore (BuildRow (..), BuildStore (..))
 import Server.Domain (BuildId (..), BuildState (..), BuildSummary (..))
 import Server.Service (BuildService (..))
 import Server.Service.Persistent (makePersistentService)
@@ -14,10 +14,32 @@ import Test.HUnit (Test (TestCase, TestLabel, TestList), (@?=))
 tests :: Test
 tests =
   TestList
-    [ TestLabel "given a store and a missing build id, getBuildSummary returns Nothing" $ TestCase $ do
-        let store = BuildStore {demo = const "demo"}
-        let service = makePersistentService store
-        actual <- getBuildSummary service (BuildId "123")
-        let expected = Nothing
-        actual @?= expected
+    [ TestLabel "given a store and a non-existent build id, getBuildSummary returns Nothing" testGetBuildSummaryNonExistent,
+      TestLabel "given a store that returns two rows for a build id, getBuildSummary returns a summary" testGetBuildSummaryTwoRows
     ]
+
+defaultStore :: BuildStore
+defaultStore = BuildStore {getBuildRows = undefined}
+
+testGetBuildSummaryNonExistent :: Test
+testGetBuildSummaryNonExistent = TestCase $ do
+  let service = makePersistentService defaultStore
+  actual <- getBuildSummary service (BuildId "123")
+  let expected = Nothing
+  actual @?= expected
+
+testGetBuildSummaryTwoRows :: Test
+testGetBuildSummaryTwoRows = TestCase $ do
+  let service =
+        makePersistentService
+          defaultStore
+            { getBuildRows =
+                const $
+                  pure
+                    [ BuildRow {buildId = "123", versionId = "04a66b1n", cadence = "slow", state = "init"},
+                      BuildRow {buildId = "123", versionId = "04a66b1n", cadence = "fast", state = "running"}
+                    ]
+            }
+  actual <- getBuildSummary service (BuildId "123")
+  let expected = Just $ BuildSummary {slowState = Init, fastState = Running}
+  actual @?= expected

dcd9d4c144ea22f34b7771dabc893b74c47838ef
Erik T. Madsen <beatmadsen@gmail.com>
2025-02-15 16:44:15 +0100

    Passing tmpdir tests

--- src/Server/DataStore/TmpDir.hs
+++ src/Server/DataStore/TmpDir.hs
@@ -1,6 +1,11 @@
 module Server.DataStore.TmpDir where
 
 import System.Directory
+import System.FilePath ((</>))
 
 tmpDir :: String -> IO FilePath
-tmpDir = undefined
+tmpDir subdir = do
+  tmp <- getTemporaryDirectory
+  let path = tmp </> "left-ci" </> subdir
+  createDirectoryIfMissing True path
+  return path
--- test/integration/Server/DataStore/TmpDirTest.hs
+++ test/integration/Server/DataStore/TmpDirTest.hs
@@ -1,20 +1,75 @@
-module Server.DataStore.TmpDirTest 
-( tests,
+module Server.DataStore.TmpDirTest
+  ( tests,
   )
 where
 
-import Test.HUnit
-import System.Directory (doesDirectoryExist)
+import Control.Exception (bracket)
 import Server.DataStore.TmpDir
+import System.Directory (doesDirectoryExist, removeDirectoryRecursive, doesFileExist)
+import Test.HUnit
+import System.FilePath ((</>))
+import Control.Monad (replicateM)
+import System.Random (randomRIO)
 
 
 tests :: Test
-tests = TestList
-    [ TestLabel "Given a subdirectory, when creating a tmpdir, then the subdirectory is also created" testCreation]
+tests =
+  TestList
+    [ TestLabel "Given a subdirectory, when creating a tmpdir, then the subdirectory is also created" testCreation,
+      TestLabel "Given a subdirectory, when creating a tmpdir twice, then the operation is idempotent" testIdempotency,
+      TestLabel "Given a tmpdir, we can create a file in it" testFileCreation
+    ]
 
 testCreation :: Test
-testCreation = TestCase $ do
-  path <- tmpDir "test-subdir"
-  actual <- doesDirectoryExist path
-  assertEqual "Subdirectory is created" True actual
+testCreation =
+  TestCase $
+    bracket
+      (getUniqueDirName >>= tmpDir) -- setup, outputs path
+      removeDirectoryRecursive -- teardown, removes path from setup
+      ( \path -> do
+          -- test, uses the path from setup
+          actual <- doesDirectoryExist path
+          assertEqual "Subdirectory is created" True actual
+      )
+
+testIdempotency :: Test
+testIdempotency =
+  TestCase $
+    bracket
+      getDirTwice -- setup, outputs path
+      removeDirectoryRecursive -- teardown, removes path from setup
+      ( \path -> do
+          -- test, uses the path from setup
+          actual <- doesDirectoryExist path
+          assertEqual "Subdirectory is created" True actual
+      )
+  where
+    getDirTwice = do
+      sameSubDir <- getUniqueDirName
+      tmpDir sameSubDir
+      path2 <- tmpDir sameSubDir
+      return path2
+
+testFileCreation :: Test
+testFileCreation = TestCase $ bracket
+  (getUniqueDirName >>= tmpDir) -- setup, outputs path
+  removeDirectoryRecursive -- teardown, removes path from setup
+  (\path -> do
+    let filePath = path </> "test.txt"
+    createFile filePath
+    actual <- doesFileExist filePath
+    assertEqual "File is created" True actual
+  )
+
+getUniqueDirName :: IO String
+getUniqueDirName = do
+  replicateM 10 $ do
+    i <- randomRIO (0, length alphabet - 1)
+    return $ alphabet !! i
+  where
+    alphabet = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+
+createFile :: FilePath -> IO ()
+createFile path = do
+  writeFile path "test"
 

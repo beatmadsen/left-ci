a68aafb5e9fba10104d4b34214251d0c3927eb07
Erik T. Madsen <beatmadsen@gmail.com>
2025-02-15 21:27:10 +0100

    Working database setup

--- src/Server/DataStore/SQLiteSetup.hs
+++ src/Server/DataStore/SQLiteSetup.hs
@@ -1,31 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
-module Server.DataStore.SQLiteSetup (
-  SQLiteClient,
-  initSQLiteDatabase
-) where
+module Server.DataStore.SQLiteSetup
+  ( SQLiteClient,
+    initSQLiteDatabase,
+  )
+where
 
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
+import Database.SQLite.Simple.QQ (sql)
 import Database.SQLite.Simple.ToRow
 import Server.DataStore.TmpDir
+import System.Directory (doesFileExist, removeDirectoryRecursive)
 import System.FilePath ((</>))
-
-import System.Directory (removeDirectoryRecursive, doesFileExist)
 
 -- A SQLiteClient is a function that can create fresh connections when needed
 type SQLiteClient = IO Connection
 
 initSQLiteDatabase :: FilePath -> IO (FilePath, FilePath, SQLiteClient)
 initSQLiteDatabase subDir = do
-  -- First, create an initial connection to set up the database
   dbDir <- tmpDir subDir
-  let dbPath = dbDir </> "left-ci.db"
-  conn <- openConnection dbPath 
-  -- create versions table
-  execute_ conn "CREATE TABLE IF NOT EXISTS versions (id INTEGER PRIMARY KEY, commit_hash TEXT NOT NULL UNIQUE, created_at DATETIME NOT NULL)"
-  
-  close conn
+  let dbPath = dbDir </> "left-ci.db"  
+  initDb dbPath
   -- Return a function that creates fresh connections
   return (dbDir, dbPath, openConnection dbPath)
 
@@ -36,3 +33,67 @@ openConnection dbPath = do
   execute_ conn "PRAGMA foreign_keys = ON"
   execute_ conn "PRAGMA journal_mode = WAL"
   return conn
+
+initDb :: FilePath -> IO ()
+initDb dbPath = do
+  conn <- openConnection dbPath
+  initTables conn
+  close conn
+
+initTables :: Connection -> IO ()
+initTables conn = do
+  createVersionsTable conn
+  createBuildsTable conn
+  createAndPopulateSuitesTable conn
+  createExecutionsTable conn
+
+createVersionsTable :: Connection -> IO ()
+createVersionsTable conn = do
+  execute_ conn [sql|
+    CREATE TABLE IF NOT EXISTS versions (
+        id INTEGER PRIMARY KEY,
+        commit_hash TEXT NOT NULL UNIQUE,
+        created_at DATETIME NOT NULL
+    )
+  |]
+
+createBuildsTable :: Connection -> IO ()
+createBuildsTable conn = do
+  execute_ conn [sql|
+    CREATE TABLE IF NOT EXISTS builds (
+        id INTEGER PRIMARY KEY,
+        version_id INTEGER NOT NULL,
+        global_id TEXT NOT NULL UNIQUE,
+        created_at DATETIME NOT NULL,
+        FOREIGN KEY (version_id) REFERENCES versions(id)
+    )
+  |]
+
+createAndPopulateSuitesTable :: Connection -> IO ()
+createAndPopulateSuitesTable conn = do
+  execute_ conn [sql|
+    CREATE TABLE IF NOT EXISTS suites (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+    )
+  |]
+  execute_ conn [sql| 
+    INSERT INTO suites (id, name) 
+    VALUES (100, 'slow'), (200, 'fast')
+   |] 
+
+createExecutionsTable :: Connection -> IO ()
+createExecutionsTable conn = do
+  execute_ conn [sql|
+    CREATE TABLE IF NOT EXISTS executions (
+        id INTEGER PRIMARY KEY,
+        build_id INTEGER NOT NULL,
+        suite_id INTEGER NOT NULL,
+        state TEXT NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        FOREIGN KEY (build_id) REFERENCES builds(id),
+        FOREIGN KEY (suite_id) REFERENCES suites(id),
+        UNIQUE(build_id, suite_id)
+    )
+  |]
--- test/integration/Server/DataStore/SQLiteSetupTest.hs
+++ test/integration/Server/DataStore/SQLiteSetupTest.hs
@@ -29,6 +29,8 @@ testInserts = TestCase $ bracket
     -- test
     connection <- client
     execute_ connection "INSERT INTO versions (commit_hash, created_at) VALUES ('abcd1234', '2021-01-01')"
+    execute_ connection "INSERT INTO builds (version_id, global_id, created_at) VALUES (1, 'global1', '2021-01-01')"
+    execute_ connection "INSERT INTO executions (build_id, suite_id, state, created_at, updated_at) VALUES (1, 100, 'Init', '2021-01-01', '2021-01-01')"
     close connection
   )  
   

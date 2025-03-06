1bd6ba95ba9c1ff133ab08bd91f279a47cb2c1c4
Erik T. Madsen <beatmadsen@gmail.com>
2025-03-07 00:42:39 +0100

    Model that updates itself based on fetching

--- site/js/lib/model.js
+++ site/js/lib/model.js
@@ -11,11 +11,11 @@ export class BuildHistory {
   async update() {
     if (Object.keys(this.builds).length === 0) {
       const builds = await this.fetchFn(this.projectId);
-      this.builds = this.parse(builds);
+      this.builds = reviveDates(builds);
     } else {
       const latestUpdate = this.#getLatestUpdate();
       const builds = await this.fetchFn(this.projectId, { after: latestUpdate });
-      this.builds = { ...this.builds, ...this.#parse(builds) };
+      this.builds = { ...this.builds, ...reviveDates(builds) };
     }
   }
 
@@ -23,13 +23,31 @@ export class BuildHistory {
     return toDataTable(this.builds);
   }
 
-  #parse(builds) {
-    // map date strings to Date objects
-    throw new Error("Not implemented");
-    
-  }
-
   #getLatestUpdate() {
-    throw new Error("Not implemented");
+    let latestUpdate = 0;
+    for (const build of Object.values(this.builds)) {
+      for (const suite of Object.values(build)) {
+        if (suite.updated_at > latestUpdate) {
+          latestUpdate = suite.updated_at;
+        }
+      }
+    }
+    return latestUpdate;
   }
+}
+
+
+export function reviveDates(builds) {
+  return Object.fromEntries(Object.entries(builds).map(([buildKey, suites]) => [buildKey, reviveDatesInSuites(suites)]));
+}
+
+function reviveDatesInSuites(suites) {
+  return Object.fromEntries(Object.entries(suites).map(([suiteName, suite]) => [suiteName, reviveDatesInSuite(suite)]));
+}
+
+function reviveDatesInSuite(suite) {
+  const cloned = { ...suite };
+  cloned.created_at = new Date(cloned.created_at);
+  cloned.updated_at = new Date(cloned.updated_at);
+  return cloned;
 }
--- site/js/tests/model.test.js
+++ site/js/tests/model.test.js
@@ -1,4 +1,4 @@
-import { BuildHistory } from "../lib/model";
+import { BuildHistory, reviveDates } from "../lib/model";
 import { jest } from "@jest/globals";
 
 const archetypeData = {
@@ -52,4 +52,28 @@ test("given a history with some builds, when updating, then the history is updat
   await history.update();
   await history.update();
   expect(history.rows()).toHaveLength(6);
+})
+
+
+test("given a history with some builds, when updating, then we fetch the builds after the latest build", async () => {
+  const firstUpdate = { ...archetypeData, "abc": { ...archetypeData["abc"], "slow_suite": { ...archetypeData["abc"]["slow_suite"], "updated_at": "2025-03-22T20:48:43.193578Z" } } };
+  const secondUpdate = {}
+
+  const fetchFn = jest.fn();
+  fetchFn.mockResolvedValueOnce(firstUpdate);
+  fetchFn.mockResolvedValueOnce(secondUpdate);
+  const history = new BuildHistory("my-project", fetchFn);
+  await history.update();
+  await history.update();
+  expect(fetchFn).toHaveBeenCalledWith("my-project", { after: new Date("2025-03-22T20:48:43.193578Z") });
+})
+
+test("given fetched builds, when parsing, then dates are revived", () => {
+  const builds = {
+    "abc": {
+      "fast_suite": { "created_at": "2025-02-22T10:44:40.160377Z", "updated_at": "2025-02-22T10:44:40.160377Z" }
+    }
+  }
+  const parsed = reviveDates(builds);
+  expect(parsed).toEqual({ "abc": { "fast_suite": { "created_at": new Date("2025-02-22T10:44:40.160377Z"), "updated_at": new Date("2025-02-22T10:44:40.160377Z") } } });
 })

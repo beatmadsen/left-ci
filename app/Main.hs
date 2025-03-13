655932b3b5151669d497bae7be142903b2a53530
Erik T. Madsen <beatmadsen@gmail.com>
2025-03-13 23:52:11 +0100

    More stable CTRL-C operation

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
-import Config.ApplicationConfig (parseApplicationConfig, ApplicationConfig (..))
+import Config.ApplicationConfig (ApplicationConfig (..), parseApplicationConfig)
 import Config.Validator (validate)
-import System.Environment (getArgs)
-import Server (makeWaiApp)
-import Network.Wai.Handler.Warp (run, runSettings, defaultSettings, setPort, setInstallShutdownHandler)
-import System.Posix.Signals (installHandler, sigINT, Handler(Catch))
-import Control.Concurrent (myThreadId, killThread)
+import Control.Concurrent (forkIO, killThread, myThreadId)
+import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
 import qualified Control.Exception as E
 import Control.Monad (void)
+import Network.Wai.Handler.Warp (defaultSettings, run, runSettings, setInstallShutdownHandler, setPort)
+import Server (makeWaiApp)
+import System.Environment (getArgs)
+import System.Posix.Signals (Handler (Catch), installHandler, sigINT)
 
 main :: IO ()
 main = do
@@ -19,24 +20,43 @@ main = do
     _ -> print v
 
 closeSocketOnSIGINT :: IO () -> IO ()
-closeSocketOnSIGINT closeSocket = void $ installHandler sigINT (Catch $ do
-    putStrLn "\nReceived Ctrl+C, shutting down..."
-    closeSocket
-    -- Force exit if the server doesn't shut down cleanly
-    tid <- myThreadId
-    killThread tid
-  ) Nothing
+closeSocketOnSIGINT closeSocket =
+  void $
+    installHandler
+      sigINT
+      ( Catch $ do
+          putStrLn "\nReceived Ctrl+C, shutting down..."
+          closeSocket
+          -- Force exit if the server doesn't shut down cleanly
+          tid <- myThreadId
+          killThread tid
+      )
+      Nothing
 
 runServer :: Int -> IO ()
 runServer port = do
   putStrLn "Starting server... (Press Ctrl+C to stop)"
   app <- makeWaiApp "data"
   putStrLn $ "Running server on port " ++ show port
-  
-  let settings = setPort port $ 
-                 setInstallShutdownHandler closeSocketOnSIGINT defaultSettings
-                 
-  E.bracket
-    (pure ())
-    (\_ -> putStrLn "Server shutdown complete")
-    (\_ -> runSettings settings app)
+
+  -- Create shutdown signal
+  shutdownMVar <- newEmptyMVar
+
+  -- Install signal handler that can interrupt request processing
+  void $
+    installHandler
+      sigINT
+      ( Catch $ do
+          putStrLn "\nReceived Ctrl+C, shutting down..."
+          putMVar shutdownMVar ()
+      )
+      Nothing
+
+  -- Run server in separate thread
+  let settings = setPort port defaultSettings
+  serverThread <- forkIO $ runSettings settings app
+
+  -- Wait for shutdown signal
+  takeMVar shutdownMVar
+  killThread serverThread
+  putStrLn "Server shutdown complete"
